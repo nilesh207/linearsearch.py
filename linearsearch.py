8012a518cb5f406542bb5382d8b70c418083c19d
NILESH SONKUSARE <69088322+nilesh207@users.noreply.github.com>
2020-08-23 01:00:49 +0530

    Create linearsearch.py

---
+++ linearsearch.py
@@ -0,0 +1,13 @@
+def search(list, n):
+  for i in range(len(list)-1):
+    if list[i] == n:
+        return True
+        
+  return False
+  
+list = [22,11,99,44,55,66,77,33,88]
+n = 77
+if search(list, n):
+  print("Found at ",pos)
+else:
+  print("Not Found")
